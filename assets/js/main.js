687209ce2f39131747a92cfe7f5c61b671f89ca6
pcferri <pedro@cybersquare.com.br>
2021-12-02 11:42:00 -0300

    Ajuste de https para video

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,7 +210,17 @@ if (typeof MauticSDKLoaded == 'undefined') {
 
 
 
-
+$.extend(true, $.magnificPopup.defaults, {  
+    iframe: {
+        patterns: {
+           youtube: {
+              index: 'youtube.com/', 
+              id: 'v=', 
+              src: 'https://www.youtube.com/embed/%id%?autoplay=1' 
+          }
+        }
+    }
+});
 
 
 
